959fde758dbb0f040a64bbbf306df2fb8f75383d
abakarmagomedov <skat131998@mail.ru>
2018-01-11 16:42:27 +0300

    work with dagger

--- app/src/main/java/com/smedialink/abakarmagomedov/musicapplication/App.kt
+++ app/src/main/java/com/smedialink/abakarmagomedov/musicapplication/App.kt
@@ -1,6 +1,7 @@
 package com.smedialink.abakarmagomedov.musicapplication
 
 
+import com.smedialink.abakarmagomedov.musicapplication.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
