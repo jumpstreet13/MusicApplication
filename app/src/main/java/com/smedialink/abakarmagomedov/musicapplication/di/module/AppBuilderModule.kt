41d91c9c079d37a40525dcaf83897c07699fe787
abakarmagomedov <skat131998@mail.ru>
2018-01-12 14:42:48 +0300

    changes in resources of project

--- app/src/main/java/com/smedialink/abakarmagomedov/musicapplication/di/module/AppBuilderModule.kt
+++ app/src/main/java/com/smedialink/abakarmagomedov/musicapplication/di/module/AppBuilderModule.kt
@@ -1,8 +1,16 @@
 package com.smedialink.abakarmagomedov.musicapplication.di.module
 
+import com.smedialink.abakarmagomedov.musicapplication.di.scope.PerActivity
+import com.smedialink.abakarmagomedov.musicapplication.presentation.feature.player.di.PlayerViewModule
+import com.smedialink.abakarmagomedov.musicapplication.presentation.feature.player.view.impl.PlayerActivity
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
 interface AppBuilderModule {
+
+    @PerActivity
+    @ContributesAndroidInjector(modules = arrayOf(PlayerViewModule::class))
+    fun providePlayerActivityFactory(): PlayerActivity
 
 }
--- app/src/main/java/com/smedialink/abakarmagomedov/musicapplication/presentation/feature/player/di/PlayerViewModule.kt
+++ app/src/main/java/com/smedialink/abakarmagomedov/musicapplication/presentation/feature/player/di/PlayerViewModule.kt
@@ -1,6 +1,8 @@
 package com.smedialink.abakarmagomedov.musicapplication.presentation.feature.player.di
 
+import com.smedialink.abakarmagomedov.musicapplication.presentation.feature.player.view.impl.PlayerActivity
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
 class PlayerViewModule {
